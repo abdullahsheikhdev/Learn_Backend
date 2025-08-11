e259e0305ca155274c9ff80dcb7f1d2667e912ce
Abdullah <mbabdullahsk4220@gmail.com>
2025-08-12 00:40:59 +0600

    setup backend project

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
-const users = [
-    {id:1, name:'Shobuj', email:'sobuj@email.com'},
-    {id:2, name:'Sohun', email:'sohun@email.com'},
-    {id:3, name:'Jesan', email:'jesan@email.com'},
-]
+
 
 app.get('/', (req, res) => {
-  res.send('Hello jan!',users)
+  res.send('Hello jan!')
 })
 
-app.get('/users', (req, res) => {
-    res.send(users)
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
