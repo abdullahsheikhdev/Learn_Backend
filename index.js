7f68c88ce48c156ed26901a6ddd74276085cb508
Abdullah <mbabdullahsk4220@gmail.com>
2025-08-18 22:24:53 +0600

    setup the new backend project

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 //ok
+//plese
 
 const uri =
   "mongodb+srv://indgm07:dADpnxaMugeDWPDF@learn.je1vlcp.mongodb.net/?retryWrites=true&w=majority&appName=learn";
